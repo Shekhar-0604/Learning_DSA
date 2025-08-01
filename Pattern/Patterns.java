04cb023c321224ec2adab8305b57106f1a5d1017
Shekhar <Shekhar209203@gmail.com>
2025-08-01 23:04:30 +0530

    Learning

--- Pattern/Patterns.java
+++ Pattern/Patterns.java
@@ -1,25 +1,24 @@
 import java.util.Scanner;
-/*
-Code to print this Pattern
-1
-12
-123
-1234
-12345
- */
 
-public class Pattern5 {
+public class Patterns {
     public static void main(String[] argc){
         Scanner sc = new Scanner(System.in);
             int n = sc.nextInt();
-            printPattern(n);
+            Pattern6(n);
             sc.close();
     }
-
-    private static void printPattern(int n) {
+    public static void Pattern6(int n){
+        /*
+          this method will print this pattern
+            1
+            22
+            333
+            4444
+            55555
+         */
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){ 
-                System.out.print(j);
+                System.out.print(i);
             }
             System.out.println();
         }
