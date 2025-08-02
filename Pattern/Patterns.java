f1c35588dabfd211bf1d1cc305a90d4b3ad5e77e
Shekhar <Shekhar209203@gmail.com>
2025-08-02 22:09:47 +0530

    Learning

--- Pattern/Patterns.java
+++ Pattern/Patterns.java
@@ -4,7 +4,8 @@ public class Patterns {
     public static void main(String[] argc){
         Scanner sc = new Scanner(System.in);
             int n = sc.nextInt();
-            Pattern6(n);
+            //Pattern6(n);
+            Pattern7(n);
             sc.close();
     }
     public static void Pattern6(int n){
@@ -19,6 +20,22 @@ public class Patterns {
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){ 
                 System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+    public static void Pattern7(int n){
+        /*
+          this method will print this pattern
+            1
+            12
+            123
+            1234
+            12345
+         */
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){ 
+                System.out.print(j);
             }
             System.out.println();
         }
