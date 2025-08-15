4e894a4a8e434f5d98a83646e4f2a208c2ce776d
Shekhar <Shekhar209203@gmail.com>
2025-08-15 23:19:00 +0530

    Learning

--- LinkedList/SimpleLinkedList.java
+++ LinkedList/SimpleLinkedList.java
@@ -1,15 +1,16 @@
-class Node {
-    int data;
-    Node next;
+public class SimpleLinkedList {
 
-    Node(int x) {
-        data = x;
-        next = null;
-    }
-}
-
-class LinkedList {
     Node head;
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int x) {
+            data = x;
+            next = null;
+        }
+    }
 
     public void printlist() {
         Node n = head;
@@ -18,12 +19,9 @@ class LinkedList {
             n = n.next;
         }
     }
-}
-
-public class SimpleLinkedList {
 
     public static void main(String[] args) {
-        LinkedList llist = new LinkedList();
+        SimpleLinkedList llist = new SimpleLinkedList();
         llist.head = new Node(5);
         Node head1 = new Node(10);
         Node head2 = new Node(10);
